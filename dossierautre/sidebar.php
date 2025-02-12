f1c01dbaf0645aa4a94159df49d32aaf29c815eb
akouya <kone@gmail.com>
2025-02-12 14:53:13 +0000

    les autre dossier

---
+++ dossierautre/sidebar.php
@@ -0,0 +1,71 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+  <ul class="nav">
+    <li class="nav-item">
+      <a class="nav-link" href="index.php">
+        <i class="icon-grid menu-icon"></i>
+        <span class="menu-title">Accueil</span>
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+        <i class="icon-layout menu-icon"></i>
+        <span class="menu-title">Personnes</span>
+        <i class="menu-arrow"></i>
+      </a>
+      <div class="collapse" id="ui-basic">
+        <ul class="nav flex-column sub-menu">
+          <li class="nav-item"> <a class="nav-link" href="dossierpersonne.php">Liste personne</a></li>
+          
+        </ul>
+      </div>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" data-bs-toggle="collapse" href="#form-elements" aria-expanded="false" aria-controls="form-elements">
+        <i class="icon-columns menu-icon"></i>
+        <span class="menu-title">Application</span>
+        <i class="menu-arrow"></i>
+      </a>
+      <div class="collapse" id="form-elements">
+        <ul class="nav flex-column sub-menu">
+          <li class="nav-item"><a class="nav-link" href="dossierapplication.php">Liste des Application</a></li>
+          
+        </ul>
+      </div>
+    </li>
+
+    
+
+    <li class="nav-item">
+      <a class="nav-link" data-bs-toggle="collapse" href="#charts" aria-expanded="false" aria-controls="charts">
+        <i class="icon-bar-graph menu-icon"></i>
+        <span class="menu-title">Paramèttres</span>
+        <i class="menu-arrow"></i>
+      </a>
+      <div class="collapse" id="charts">
+        <ul class="nav flex-column sub-menu">
+          <li class="nav-item"> <a class="nav-link" href="dossierarchitecture.php"> Architectures</a></li>
+          <li class="nav-item"> <a class="nav-link" href="dossierniveau.php"> Niveau_couche</a></li>
+          <li class="nav-item"> <a class="nav-link" href="dossiermode.php">Mode_deploiement</a></li>
+          <li class="nav-item"> <a class="nav-link" href="dossiertypersonne.php">Typepersonne</a></li>
+        </ul>
+      </div>
+    </li>
+    
+        <li class="nav-item">
+      <a class="nav-link" data-bs-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
+        <i class="icon-head menu-icon"></i>
+        <span class="menu-title">Voir les informations</span>
+        <i class="menu-arrow"></i>
+      </a>
+      <div class="collapse" id="auth">
+        <ul class="nav flex-column sub-menu">
+          <li class="nav-item"> <a class="nav-link" href="dossierrechecher.php.php"> enregistrer des personnes</a></li>
+          <li class="nav-item"> <a class="nav-link" href="dossierrechecher.php"> Rechercher des personnes</a></li>
+          <li class="nav-item"> <a class="nav-link" href="dossierrechecher.php"> Affichage des personnes et application</a></li>
+        </ul>
+        </div>
+    </li>
+    
+    
+  </ul>
+</nav>
